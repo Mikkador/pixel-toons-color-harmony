430e88decb505dd5e576dc64162387d29320a340
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-06-26 11:12:23 +0200

    color to rgb implement

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,15 @@
+const getColor = (string) => {
+  const r = parseInt(string.substr(1, 2), 16);
+  const g = parseInt(string.substr(3, 2), 16);
+  const b = parseInt(string.substr(5, 2), 16);
+  return { r, g, b };
+};
+
 window.onload = () => {
   const picker = document.getElementById('pick-color');
   const output = document.getElementById('pick-output');
   picker.oninput = () => {
-    output.innerText = picker.value;
+    const color = getColor(picker.value);
+    output.innerText = `${color.r} ${color.g} ${color.b}`;
   };
 };
