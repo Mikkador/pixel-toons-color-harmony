bc7a9c3954e712542903427f06f7154ed74e1f7b
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-06-26 11:08:57 +0200

    color picking done

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,7 @@
 window.onload = () => {
-  console.log('Example loaded successfully!');
+  const picker = document.getElementById('pick-color');
+  const output = document.getElementById('pick-output');
+  picker.oninput = () => {
+    output.innerText = picker.value;
+  };
 };
