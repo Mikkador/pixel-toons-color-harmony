32dafb292ed134421ef19f97b5c41069d1647f25
Daria Petruk <90788327+Mikkador@users.noreply.github.com>
2022-06-26 11:38:15 +0200

    hsl implemented

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -5,11 +5,39 @@ const getColor = (string) => {
   return { r, g, b };
 };
 
+const toHsl = (color) => {
+  const r1 = color.r / 255;
+  const g1 = color.g / 255;
+  const b1 = color.b / 255;
+  const max = Math.max(r1, g1, b1);
+  const min = Math.min(r1, g1, b1);
+  const delta = max - min;
+  let hue;
+  if (delta === 0) {
+    hue = 0;
+  } else if (max === r1) {
+    hue = 60 * (((g1 - b1) / delta) % 6);
+  } else if (max === g1) {
+    hue = 60 * (((b1 - r1) / delta) + 2);
+  } else {
+    hue = 60 * (((r1 - g1) / delta) + 4);
+  }
+  const lightness = (max + min) / 2;
+  let saturation;
+  if (delta === 0) {
+    saturation = 0;
+  } else {
+    saturation = delta / (1 - Math.abs(2 * lightness - 1));
+  }
+  return { hue, saturation, lightness };
+};
+
 window.onload = () => {
   const picker = document.getElementById('pick-color');
   const output = document.getElementById('pick-output');
   picker.oninput = () => {
     const color = getColor(picker.value);
     output.innerText = `${color.r} ${color.g} ${color.b}`;
+    console.log(toHsl(color));
   };
 };
